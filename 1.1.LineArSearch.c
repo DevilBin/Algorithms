7d3ebf4cb504a9e3182794c03b96e3da1e2d6856
DevilBin <whu.evil.binary@gmail.com>
2015-08-28 20:29:45 +0800

    init

---
+++ 1.1.LineArSearch.c
@@ -0,0 +1,24 @@
+/*************************************************************************
+	> File Name: 1.1.LineArSearch.c
+	> Author: DevilBin 
+	> Mail: whu.evil.binary@gmail.com 
+	> Created Time: Wed 26 Aug 2015 08:46:12 PM CST
+ ************************************************************************/
+
+#include <stdio.h>
+
+int A[15] = {0, 1, 4, 5, 7, 8, 9, 10, 12, 15, 22, 23, 27, 32, 35};
+
+int main()
+{
+	int x; int j = 1;
+	scanf("%d", &x);
+	while(j < 15 && x != A[j])
+	{
+		j = j + 1;
+	}
+	if(x == A[j])
+		return j;
+	else
+		return 0;
+}
---
+++ 1.6.BottomUpSort.c
@@ -0,0 +1,80 @@
+/*************************************************************************
+	> File Name: 1.6.BottomUpSort.c
+	> Author: DevilBin 
+	> Mail: whu.evil.binary@gmail.com 
+	> Created Time: Fri 28 Aug 2015 03:32:59 PM CST
+ ************************************************************************/
+
+#include <stdio.h> 
+#include <stdlib.h>
+#define N 8
+
+int A[9] = {0, 9, 4, 5, 2, 1, 7, 4, 6};
+int B[9] = {};
+int Merge(int *A, int p, int q, int r)
+{
+	int s, t, k;
+	s = p; t = q + 1; k = p;
+	while(s <= q && t <=r)
+	{
+		if(A[s] <= A[t])
+		{
+			B[k] = A[s];
+			s = s + 1;
+		}
+		else
+		{
+			B[k] = A[t];
+			t = t + 1;
+		}
+		k = k + 1;
+	}
+	if(s == q + 1)
+	{
+		for(;t <= r;)
+		{
+			B[k] = A[t];
+			k = k + 1;
+			t = t + 1;
+		}
+	}
+	else
+	{
+		for(;s <= q;)
+		{
+			B[k] = A[s];
+			k = k + 1;
+			s = s + 1;
+		}
+	}
+	for(;p <= r;)
+	{
+		A[p] = B[p];
+		p = p + 1;
+	}
+	return 0;
+}
+
+int main()
+{
+	int t, i, s;
+	t = 1;
+	while(t < N)
+	{
+		s = t;
+		i = 0;
+		t = 2 * s; 
+		while(i + t <= N)
+		{
+			Merge(A, i + 1, i + s, i + t);
+			i = i + t;
+		}
+		if(i + s < N)
+			Merge(A, i + 1, i + s, N);
+	}
+	for(i = 0; i <= N; i ++)
+	{
+		printf("%d ", A[i]);
+	}
+	return 0;
+}
