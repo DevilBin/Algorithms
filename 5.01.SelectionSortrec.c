445936a6840b37927b102199be05fb3422ecf6c1
DevilBin <whu.evil.binary@gmail.com>
2015-09-01 20:07:42 +0800

    push files

--- 5.01.SelectionSortrec.c
+++ 5.01.SelectionSortrec.c
@@ -1,37 +1,44 @@
 /*************************************************************************
-	> File Name: 1.04.SelectionSort.c
+	> File Name: 5.01.SelectionSortrec.c
 	> Author: DevilBin 
 	> Mail: whu.evil.binary@gmail.com 
-	> Created Time: Fri 28 Aug 2015 07:15:58 AM CST
+	> Created Time: Tue 01 Sep 2015 07:54:39 PM CST
  ************************************************************************/
 
 #include <stdio.h> 
 #include <stdlib.h>
 #define N 14
 
+int n;
 int A[15] = {0, 17, 28, 10, 4, 1, 22, 15, 27, 23, 5, 32, 12, 39, 35};
 
-int main()
+void SeleSort(int i)
 {
-	int i, j, k; int temp;
-	for(i = 1;i < N; ++i)
+	int j, k, temp;
+	if(i < n)
 	{
 		k = i;
-		for(j = i + 1;j <= N; ++j)
+		for(j = i + 1; j <= n; ++j)
 		{
 			if(A[j] < A[k])
-			{
 				k = j;
-			}
 		}
-		if(k != i)
+		if(k != j)
 		{
 			temp = A[i];
 			A[i] = A[k];
 			A[k] = temp;
 		}
+		SeleSort(i + 1);
 	}
-	for(i = 1;i <= N; ++i)
+}
+
+int main()
+{
+	int i;
+	n = N;
+	SeleSort(1);
+	for(i = 1; i <= n; ++i)
 	{
 		printf("%d ", A[i]);
 	}
